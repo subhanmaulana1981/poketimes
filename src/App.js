9856f16790e63a2c628f1368e7b9e5471cda943a
subhanmaulana1981 <subhanmaulana1981@gmail.com>
2022-08-11 20:46:12 +0700

    link to pages finally found

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NoPage from './components/NoPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='*' element={<NoPage />} />      
+        </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NoPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoPage = () => {
+    return (
+        <view className="container center">
+            <h1>404</h1>
+            <p>The page you are trying to reach is unAvailable</p>
+        </view>
+    )
+}
+
+export default NoPage
